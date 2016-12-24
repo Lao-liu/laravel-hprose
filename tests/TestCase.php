4f540766026dd939cf098a5b24e083924dc20a98
Laoliu <Laoliu@lanmv.com>
2016-12-24 19:35:00 +0800

    Add test

---
+++ tests/TestCase.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Foundation\AliasLoader;
+
+class TestCase extends Illuminate\Foundation\Testing\TestCase {
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../vendor/laravel/laravel/bootstrap/app.php';
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        $this->registerServiceProviders($app);
+
+        return $app;
+    }
+
+    protected function registerServiceProviders($app) {
+        $app->register('Laoliu\LaravelHprose\HproseServiceProvider');
+    }
+}
